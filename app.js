df1f26e5848a722fb87fc3321374ffbd17a4025b
Kiran F <kfatima11294@gmail.com>
2023-01-06 13:00:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -38,16 +38,51 @@ axious
 //ASYNC/AWAIT METHOD:
 //If you're looking for a specific person, then you put he id within
 //the parameter
-const getStarWarsPerson = async (id) => {
-  try {
-    //1
-    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-    console.log(res.data);
-    //2
-  } catch (e) {
-    console.log("ERROR", e);
-  }
-};
+// const getStarWarsPerson = async (id) => {
+//   try {
+//     //1
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//     console.log(res.data);
+//     //2
+//   } catch (e) {
+//     console.log("ERROR", e);
+//   }
+// };
 
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+
+/**** HEADERS WITH AXIOS *****/
+//Challenge: Add each new joke to the page inside the ul
+
+const jokeList = document.querySelector('#jokes');
+const button = document.querySelector('button');
+const getDadJoke = async() => {
+  //const res = await axios.get('https://icanhazdadjoke.com/');
+  //console.log(res); <-- this returns html if you look in the data key
+  //if we want JSON for this particular API we have to include a header:
+  const config = { headers: {Accept: 'application/json'}} //this wording is something found in the docs of the API
+  const res = await axios.get('https://icanhazdadjoke.com/', config);
+  //we could include the header directly after the url, but it looks kinda messy
+  console.log(res); //this returns JSON in the data key
+
+  //Extract the joke out of the data, and return it so we can save it in a variable
+  //when we call it in the next function
+  return res.data.joke;
+}
+
+//function for adding a new joke
+const addNewJoke = () => {
+  //Call the API request function
+  const jokeText = getDadJoke()
+  //Create an li
+  const newLi = document.createElement('LI');
+  //appending the text from the API call to the li
+  newLi.append(res.data.joke)
+  //append that li to the ul
+  jokeList.append(newLi);
+}
+
+//add event listener to the button
+button.addEventListener('click', )
