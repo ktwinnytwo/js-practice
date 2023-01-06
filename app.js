1d7a8d2f16d436d334063b640ed51c58637e547e
Kiran F <kfatima11294@gmail.com>
2023-01-06 11:13:50 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// NOT AT ALL IMPORTANT TO REMEMBER ANY OF THIS CODE!
+// Old syntax, not really used anymore
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+  console.log("IT LOADED!!");
+  const data = JSON.parse(this.responseText);
+  console.log(data.name, data.height);
+};
+
+req.onerror = function () {
+  console.log("ERROR!!!!");
+  console.log(this);
+};
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
