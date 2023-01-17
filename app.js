6320ae5a4e4e6904030f9c1b5625789f35d4a99b
Kiran F <kfatima11294@gmail.com>
2023-01-17 12:15:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+/* OOP
+
+Example:
+say you create a class for Color.
+If you create a new color class:
+
+const logoColor = new Color('emerald', [46,204,113])
+
+Now you have access to the methods in the Color class.
+It contains methods to let you do the following:
+ - calculate the opposite color
+ - calculate the hex code for the color
+ - give you a slightly lighter version of your color
+ - give you a slightyl darker version of your color
+
+ Now everytime you create a new class of Color, all of them will have
+ access to the methods in that class.
+*/
