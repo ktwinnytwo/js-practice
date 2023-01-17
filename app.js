f19068239cf7cab74ec77fc2c3fc375d487a5e24
Kiran F <kfatima11294@gmail.com>
2023-01-17 09:30:18 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+/* Prototypes are template objects.
+
+Ex: Arrays do not have a method attached to them called .length or .push() 
+if you look at their properties. Instead all of these methods are attached
+to the prototype, and they all call the same prototype instead of having to
+attach all those methods individually to each array.
+
+We can also add our own methods to __proto__ objects. 
+
+We can also overwrite an already existing method like .push but not a good idea
+
+string is also a prototype (which is where indexOf and toUppercase come from)
+
+*/	 
+
+
+
+//String.prototype is a "template object" for every single string.
+//We could go crazy and add our own method called yell...
+String.prototype.yell = function() {
+	return `OMG!!! ${this.toUpperCase()}!!!!! AGHGHGHG!`;
+};
+
+'bees'.yell(); //"OMG!!! BEES!!!!! AGHGHGHG!"
+
+//We can overwrite an existing Array method like pop (not a good idea):
+Array.prototype.pop = function() {
+	return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!';
+};
+const nums = [ 6, 7, 8, 9 ];
+nums.pop(); // "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!"
